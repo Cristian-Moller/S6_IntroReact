0a81b3d445d280824c41bac68b0c4a9b12d89e3c
Cristian Moller <mollercristian@gmail.com>
2023-05-15 14:16:36 +0000

    Nivel 1 - Ejercicio 1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Scene from './components/scene/scene'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Scene />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/scene/scene.tsx
@@ -0,0 +1,4 @@
+
+const Scene = () => <>Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial</>;
+
+export default Scene
